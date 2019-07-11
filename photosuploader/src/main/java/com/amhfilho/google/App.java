76d8433f40bcb7c0601023f5a12252cd7a5ce9ab
Antonio Henriques <amhfilho@gmail.com>
2019-07-11 10:23:59 -0300

    security fixes

--- photosuploader/src/main/java/com/amhfilho/google/App.java
+++ photosuploader/src/main/java/com/amhfilho/google/App.java
@@ -30,32 +30,5 @@ public class App {
 
         System.out.println(String.format("Found %d files. Process completed in %s seconds", files.size(),(System.currentTimeMillis()-start)/1000));
 
-
-//        // Set up the Photos Library Client that interacts with the API
-//        //Credentials credentials =
-//        PhotosLibrarySettings settings =
-//                PhotosLibrarySettings.newBuilder()
-//                        .setCredentialsProvider(
-//                                FixedCredentialsProvider.create(UserCredentials.newBuilder()
-//                                        .setClientId("310627957446-nb5hiasf7ncq0ov0b7vp9c2ichhqfqiu.apps.googleusercontent.com")
-//                                        .setClientSecret("3wt1rqnqR1-p_zFGaiftNsNP").build()))
-//                        .build();
-//
-//        try (PhotosLibraryClient photosLibraryClient =
-//                     PhotosLibraryClient.initialize(settings)) {
-//            photosLibraryClient.listMediaItems().iterateAll().forEach(i-> System.out.println(i.getFilename()));
-//            //photosLibraryClient.uploadMediaItem()
-//            // Create a new Album  with at title
-//            //Album createdAlbum = photosLibraryClient.createAlbum("My Album");
-//
-//            // Get some properties from the album, such as its ID and product URL
-//            //String id = album.getId();
-//            //String url = album.getProductUrl();
-//
-//        } catch (ApiException e) {
-//            e.printStackTrace();
-//        }
-
-
     }
 }
