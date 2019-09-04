7ce4f5aada2c8588c9433b33687c4037533aad05
Antonio Henriques <amhfilho@gmail.com>
2019-09-04 09:16:40 -0300

    Repository cleansing

--- photosuploader/src/main/java/com/amhfilho/google/photosuploader/persistence/TargetFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amhfilho.google.photosuploader.persistence;
-
-import java.util.List;
-
-public interface TargetFileRepository {
-    List<TargetFile> findAll();
-    void save(TargetFile targetFile);
-    int deleteAll();
-}
