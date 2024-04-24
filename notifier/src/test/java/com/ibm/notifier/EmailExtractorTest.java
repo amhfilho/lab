9a23438d2cc36320a571a117ed26499f30654760
Antonio Mario Henriques Filho <amhfilho@gmail.com>
2024-04-24 11:54:58 +0200

    Update EmailExtractorTest.java

--- notifier/src/test/java/com/ibm/notifier/EmailExtractorTest.java
+++ notifier/src/test/java/com/ibm/notifier/EmailExtractorTest.java
@@ -8,9 +8,9 @@ public class EmailExtractorTest {
 
     @Test
     public void givenString_whenExtract_thenReturnValidEmail(){
-        String s = "C:\\Users\\AntonioMarioHenrique\\My Documents\\SametimeTranscripts\\amhfilho@br.ibm.com\\ChatHistory.properties\n" +
+        String s = "C:\\Users\\username\\My Documents\\SametimeTranscripts\\xxx@xxx.com\\ChatHistory.properties\n" +
                 "Fri Sep 06 10:21:36 BRT 2019";
         String email = new EmailExtractor().extract(s);
-        assertEquals("amhfilho@br.ibm.com", email);
+        assertEquals("xxx@xxx.com", email);
     }
 }
